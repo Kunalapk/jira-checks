360747c5c299786421b24adacd74ff7ab6ec8e67
kunal.pasricha <kunal.pasricha@payufin.com>
2023-06-17 15:03:47 +0530

    First commit

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ async function run() {
 	try {
 		const project_id = core.getInput('project_id');
 		const jira_token = core.getInput('token');
-		const path = './'+core.getInput('path');
+		//const path = './'+core.getInput('path');
+		const path = core.getInput('path');
 		const src = __dirname
 
 		//core.setOutput("Hello! - "+project_id+" - "+jira_token)
@@ -16,8 +17,8 @@ async function run() {
 		//const ticketId = myModule.getTicketId("sdfds");
 		//console.log("Hello! - "+project_id+" - "+jira_token.length+" - "+ticketId+" - "+path)
 
-		var lib = require('app/sauce.js')
-		console.log(lib.getTicketId("sdfds"));
+		var lib = require(path)
+		console.log(myModule.getTicketId("sdfds"));
 		//await exec.exec(`${src}/validate-jira-checks.sh -`);
 	} catch (error) {
 		core.setFailed("Failed::"+error)
