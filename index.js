4f0ba68e89171b72710968634a14182d27376f8f
kunal.pasricha <kunal.pasricha@payufin.com>
2023-06-17 13:19:14 +0530

    First commit

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ async function run() {
 	try {
 		const project_id = core.getInput('project_id');
 		const jira_token = core.getInput('token');
-		const path = './'+core.getInput('path');
+		//const path = './'+core.getInput('path');
+		const path = core.getInput('path');
 		const src = __dirname
 	  	console.log(__dirname)
 
 		//core.setOutput("Hello! - "+project_id+" - "+jira_token)
-	  	const myModule = await import(path);
-		const ticketId = myModule.getTicketId("sdfds");
-		console.log("Hello! - "+project_id+" - "+jira_token.length+" - "+ticketId+" - "+path)
+		var files = fs.readdirSync(path);
+		for (var i = 0; i < files.length; i++) {
+    		console.log(files[i]);
+		}
+	  	//const myModule = await import(path);
+		//const ticketId = myModule.getTicketId("sdfds");
+		//console.log("Hello! - "+project_id+" - "+jira_token.length+" - "+ticketId+" - "+path)
 
 		//await exec.exec(`${src}/validate-jira-checks.sh -`);
 	} catch (error) {
