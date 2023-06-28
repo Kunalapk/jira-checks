d01069c29531768b15df801817b66c919b8c46dc
kunal.pasricha <kunal.pasricha@payufin.com>
2023-06-29 02:19:25 +0530

    First commit

--- action/index.js
+++ action/index.js
@@ -11053,13 +11053,14 @@ async function run() {
 	try {
 		const project_id = core.getInput('project_id');
 		const jira_token = core.getInput('token');
-		//const path = './'+core.getInput('path');
 		const path = core.getInput('path');
+
 		var pullRequestTitle = getPullRequestTitle(github)
-		console.log("TITLEXXX: "+pullRequestTitle)
 		const { stdout } = await exec.getExecOutput(`npx run-func ${path} getTicketId "${pullRequestTitle}"`)
-		var processJiraId = stdout.toString().trim()
-		console.log("Processed JiraId - "+processJiraId+"  - ")
+		var processedJiraId = stdout.toString().trim()
+
+		var jiraTicketUrl = `https://${project_id}.atlassian.net/rest/api/latest/issue/${processedJiraId}.json`
+		console.log("jiraTicketUrl - "+jiraTicketUrl)
 	} catch (error) {
 		core.setFailed("Failed::"+error)
 	}
