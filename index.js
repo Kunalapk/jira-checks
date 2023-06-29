6d850277df91b74d35d971d35a1769e4547080f8
kunal.pasricha <kunal.pasricha@payufin.com>
2023-06-29 20:12:24 +0530

    First commit

--- index.js
+++ index.js
@@ -24,13 +24,12 @@ async function run() {
 		const github_token = core.getInput('GITHUB_TOKEN');
 
 	    const context = github.context;
-	    if (context.payload.pull_request == null) {
-	        core.setFailed('No pull request found.');
-	        return;
-	    }
+
 	    const pull_request_number = context.payload.pull_request.number;
 
-	    const octokit = new github.GitHub(github_token);
+	    //const octokit = new github.GitHub(github_token);
+	    const octokit = github.getOctokit(github_token);
+	    
 	    const new_comment = octokit.issues.createComment({
         	...context.repo,
         	issue_number: pull_request_number,
