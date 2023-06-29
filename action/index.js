c04df2bf04a4e043d166452cf55ed5051d8509ed
kunal.pasricha <kunal.pasricha@payufin.com>
2023-06-29 20:17:45 +0530

    First commit

--- action/index.js
+++ action/index.js
@@ -43850,7 +43850,7 @@ async function run() {
 	    //const octokit = new github.GitHub(github_token);
 	    const octokit = github.getOctokit(github_token);
 	    
-	    const new_comment = octokit.issues.createComment({
+	    const new_comment = octokit.rest.issues.createComment({
         	...context.repo,
         	issue_number: pull_request_number,
         	body: "hhjhgbhjgjhg"
